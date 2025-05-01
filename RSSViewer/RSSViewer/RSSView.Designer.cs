22c1b61dcc305bd6b0d931a5967598f134d4f432
Paradise072 <paradise072@icloud.com>
2025-05-01 18:09:23 -0500

    Initial Commit

---
+++ RSSViewer/RSSViewer/RSSView.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace RSSViewer
+{
+    partial class RSSView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.headlines = new System.Windows.Forms.ListBox();
+            this.ArticleView = new System.Windows.Forms.RichTextBox();
+            this.WebView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)(this.WebView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(579, 413);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(209, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Fetch Data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // headlines
+            // 
+            this.headlines.FormattingEnabled = true;
+            this.headlines.Location = new System.Drawing.Point(579, 13);
+            this.headlines.Name = "headlines";
+            this.headlines.Size = new System.Drawing.Size(208, 394);
+            this.headlines.TabIndex = 1;
+            this.headlines.SelectedIndexChanged += new System.EventHandler(this.UpdateMainView);
+            // 
+            // ArticleView
+            // 
+            this.ArticleView.Location = new System.Drawing.Point(13, 13);
+            this.ArticleView.Name = "ArticleView";
+            this.ArticleView.ReadOnly = true;
+            this.ArticleView.Size = new System.Drawing.Size(560, 83);
+            this.ArticleView.TabIndex = 2;
+            this.ArticleView.Text = "";
+            // 
+            // WebView
+            // 
+            this.WebView.AllowExternalDrop = true;
+            this.WebView.CreationProperties = null;
+            this.WebView.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.WebView.Location = new System.Drawing.Point(13, 103);
+            this.WebView.Name = "WebView";
+            this.WebView.Size = new System.Drawing.Size(560, 335);
+            this.WebView.TabIndex = 3;
+            this.WebView.ZoomFactor = 1D;
+            // 
+            // RSSView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.WebView);
+            this.Controls.Add(this.ArticleView);
+            this.Controls.Add(this.headlines);
+            this.Controls.Add(this.button1);
+            this.Name = "RSSView";
+            this.Text = "RSSView";
+            ((System.ComponentModel.ISupportInitialize)(this.WebView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox headlines;
+        private System.Windows.Forms.RichTextBox ArticleView;
+        private Microsoft.Web.WebView2.WinForms.WebView2 WebView;
+    }
+}
+
---
+++ RSSViewer/RSSViewer/RSSView.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceModel.Syndication;
+using System.Xml;
+using System.Collections.ObjectModel;
+
+namespace RSSViewer
+{
+    public partial class RSSView : Form
+    {
+
+        SyndicationFeed feed;
+
+        public RSSView()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FetchFromURL();
+        }
+        private void FetchFromURL()
+        {
+            string url = "https://feeds.npr.org/1001/rss.xml";
+
+            // I'm going to use XmlReader here following guidance from:
+            // https://stackoverflow.com/questions/10399400/best-way-to-read-rss-feed-in-net-using-c-sharp
+            XmlReader xml = XmlReader.Create(url);
+            feed = SyndicationFeed.Load(xml);
+            xml.Close();
+
+            UpdateUI(); // This could be detached and put right after function call in theory
+        }
+
+        private void UpdateUI()
+        {
+            headlines.Items.Clear();
+            foreach (SyndicationItem item in feed.Items)
+            {
+                string headline = item.Title.Text;
+                headlines.Items.Add(headline);
+            }
+        }
+
+        private string SyndicationAuthorsToString(Collection<SyndicationPerson> persons)
+        {
+            string finalString = "";
+            foreach(SyndicationPerson person in persons)
+            {
+                finalString += $"{person.Name}, ";
+            }
+            if (persons.Count > 0)
+            {
+                // Remove the last two characters (, ) from the string
+                finalString = finalString.Substring(0, finalString.Length - 2);
+            }
+            return finalString;
+        }
+
+        private void UpdateMainView(object sender, EventArgs e)
+        {
+            IEnumerable<SyndicationItem> filteredItems = feed.Items.Where((h) => h.Title.Text == headlines.Text);
+            SyndicationItem[] filteredItemsArr = filteredItems.ToArray();
+            if (filteredItemsArr.Length > 0) // We found an article!
+            {
+                SyndicationItem selectedItem = filteredItemsArr[0];
+                string publishDate = "No Publishing Date";
+                string summary = "No Summary Provided";
+                string author = "No Authors Provided";
+                string link = "No URL";
+                if (selectedItem.PublishDate != null)
+                {
+                    publishDate = selectedItem.PublishDate.ToString();
+                }
+                if (selectedItem.Summary != null)
+                {
+                    summary = selectedItem.Summary.Text;
+                }
+                if (selectedItem.Authors != null && selectedItem.Authors.Count > 0)
+                {
+                    author = SyndicationAuthorsToString(selectedItem.Authors);
+                }
+                if (selectedItem.Links != null &&  selectedItem.Links.Count > 0)
+                {
+                    Uri linkUri = selectedItem.Links[0].GetAbsoluteUri();
+                    WebView.Source = linkUri;
+                    link = linkUri.ToString();
+                }
+
+                ArticleView.Text = $"Published {publishDate} by {author}\n{link}\n\n{summary}";
+            }
+        }
+    }
+}
